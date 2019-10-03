c68f80d8591ccd1d3146ba960059aded560edf24
Hnybon <43375200+Hnybon@users.noreply.github.com>
2019-10-03 11:05:21 +0200

    aflevering

---
+++ Obl Opgave 4 Henrik Nielsen/Controllers/BogController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestLibrary;
+
+namespace Obl_Opgave_4_Henrik_Nielsen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BogController : ControllerBase
+    {
+
+        private static List<Bog> bogListe = new List<Bog>()
+        {
+            new Bog("Hans is a Freaky alien genotype", "Anders Holm", 341, "0001110001110")
+        };
+
+        // GET: api/Bog
+        [HttpGet]
+        public IEnumerable<Bog> Get()
+        {
+            return bogListe;
+        }
+
+        // GET: api/Bog/5
+        [HttpGet("{isbn13}", Name = "Get")]
+        public Bog Get(string isbn13)
+        {
+            return bogListe.Find(i => i.Isbn13 == isbn13);
+        }
+
+        // POST: api/Bog
+        [HttpPost]
+        public void Post([FromBody] Bog value)
+        {
+            bogListe.Add(value);
+        }
+
+        // PUT: api/Bog/5
+        [HttpPut("{id}")]
+        public void Put(string isbn13, [FromBody] Bog value)
+        {
+            Bog bog = Get(isbn13);
+            if (bog != null)
+            {
+                bog.Titel = value.Titel;
+                bog.Forfatter = value.Forfatter;
+                bog.Sidetal = value.Sidetal;
+                bog.Isbn13 = value.Isbn13;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{isbn13}")]
+        public void Delete(string isbn13)
+        {
+            Bog bog = Get(isbn13);
+            bogListe.Remove(bog);
+        }
+    }
+}
---
+++ RestLibrary/Bog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RestLibrary
+{
+    public class Bog
+    {
+        private string _titel;
+        private string _forfatter;
+        private int _sidetal;
+        private string _isbn13;
+
+        public Bog()
+        {
+            
+        }
+
+        public Bog(string titel, string forfatter, int sidetal, string isbn13)
+        {
+            _titel = titel;
+            _forfatter = forfatter;
+            _sidetal = sidetal;
+            _isbn13 = isbn13;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Titel)}: {Titel}, {nameof(Forfatter)}: {Forfatter}, {nameof(Sidetal)}: {Sidetal}, {nameof(Isbn13)}: {Isbn13}";
+        }
+
+        public string Titel
+        {
+            get { return _titel; }
+            set { _titel = value; }
+        }
+
+        public string Forfatter
+        {
+            get { return _forfatter; }
+            set { _forfatter = value; }
+        }
+
+        public int Sidetal
+        {
+            get { return _sidetal; }
+            set { _sidetal = value; }
+        }
+
+        public string Isbn13
+        {
+            get { return _isbn13; }
+            set { _isbn13 = value; }
+        }
+    }
+}
